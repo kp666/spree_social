7aaf37306c13deee8309b3895d12e36aca25f6d2
Krishnaprasad T Nair <krishnaprasad@mobme.in>
2012-03-26 10:16:14 +0530

    removing login through services from decorator.

--- app/overrides/user_sessions_decorator.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/user_sessions/new",
-                     :name => "add_socials_to_login_extras",
-                     :insert_after => "[data-hook='login_extras']",
-                     :partial => "spree/shared/socials",
-                     :disabled => false)
